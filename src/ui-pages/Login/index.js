f561d625e623892111ed931e3a418a09233e1095
jeyachandravega <sharaths7178@gmail.com>
2021-10-15 15:57:54 +0530

    adding 2cards in login page

--- src/ui-pages/Login/index.js
+++ src/ui-pages/Login/index.js
@@ -2,22 +2,47 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { mapDispatchToProps } from "../../ui-utils/commons";
 import { connect } from "react-redux";
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+
 
 const styles = {
   root: {
+    width: "100vw",
     height: "100vh",
-    background: "#F8F8F8",
     display: "flex",
     justifyContent: "center",
-    alignItems: "center"
   },
   container: {
-    // padding:"10%",
-    // width:"25%",
     display: "flex",
     alignItems: "center",
-    justify: "center",
-    flexDirection: "column"
+    justifyContent: "center",
+  },
+  lcardConatiner: {
+    width: "316px",
+    height: "179px",
+    backgroundColor: "#00589F",
+  },
+  rcardConatiner: {
+    width: "316px",
+    height: "179px",
+    backgroundColor: "#fff",
+
+  },
+  paragraph:{
+    textAlign: "left",
+    font: "normal normal bold 20px/23px Arial",
+    letterSpacing: "1px",
+    color: "#FFFFFF",
+    textTransform: "uppercase",
+    opacity: "1",
+  },
+  logo:{
+    background: "transparent",
+    position: "absolute",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   }
 };
 
@@ -29,9 +54,22 @@ class Login extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <div>
-        
-      </div>
+      <Grid className={classes.root}>
+        <div>
+          <img className={classes.logo} src="../ui-atoms/Images/logo-login.svg" alt="logo"/>
+        </div>
+        <Grid className={classes.container}>
+          <Card className={classes.lcardConatiner}>
+            <p className = {classes.paragraph}>WELKOM BIJ MIJN NMT</p>
+
+          </Card>
+        </Grid>
+        <Grid className={classes.container}>
+          <Card className={classes.rcardConatiner}>
+
+          </Card>
+        </Grid>
+      </Grid>
     );
   }
 }
